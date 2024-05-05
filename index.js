02d747208341aabc3a0a1ebc4947cfc1df9cd21f
Wazzydesigns <119667597+Wazzydesigns@users.noreply.github.com>
2024-05-05 21:17:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+saveEl = document.getElementById("save-el");
+countEl = document.getElementById("count-el");
+let count = 0
+
+function served() {
+    count += 1
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
